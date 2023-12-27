5252a55f61bf352a305bc31a28888a79a9fa8d09
Tasneem Hajimaming <tasnimhayimaming@gmail.com>
2023-12-27 17:54:38 +0700

    update

--- components/input.vue
+++ components/input.vue
@@ -1,9 +1,44 @@
+<script setup>
+const props = defineProps({
+    id: {
+        type: String,
+        default: "text",
+    },
+    name:{
+        type: String,
+        default: "text",
+    },
+    type: {
+        type: String,
+        default: "text",
+    },
+    placeholder: {
+        type: String,
+        default: "Enter Name",
+    },
+    value: {
+        type: String,
+    },
+    onChange: {
+        type: Function,
+    }, 
+});
+
+
+//สร้าง function ที่เป็นตัวแปร เมื่อมีการเรียกใช้ prop 
+const onchange = (e) => {
+    // console.log(e); //ไว้ดูระบบผิดพลาดยังไง //(e)จะไดค่าที่ console log ได้ค่าเปลี่ยนแปลงจากอีเว้นท์
+    // console.log("EEEE",e.target.value);
+    // console.log('porp',props.name);
+    props.onChange(e.target.value, props.name);
+};
+</script>
 <template>
     <input 
-    class="w-full h-screen rounded-lg border-2 border-solid border-gray-600
-    0.
-    0.32
-    .12" 
-    type="text" 
-    placeholder="Enter name" />
+    class="w-[300px] h-[80px] rounded-lg border-2 border-solid border-gray-600 bg-slate-600" 
+    :type="props.type"
+    :placeholder="props.placeholder" 
+    v-model="props.value"
+    @change="onchange" 
+    />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,31 @@
-<template>
-    <div class="w-full h-screen flex items-center justify-center">
+<script setup>
+const type = ref("text"); //ref เพื่อให้ค่าเปลี่ยนตามที่เราแก้ในโค้ด ถ้าไม่ใช้พำด ค่าที่เราเปลี่ยนในโค้ดจะไม่เปลี่ยนตาม
+const placeholder = ref("Enter First Name");
 
-        <Input />
+const form = reactive({
+    firstName: "First Name",
+    lastName: "Last Name",
+    Number: 100,
+});
+
+//ref
+// const boolean = true || flase
+
+const onChange = (e, name) => {
+    form[name] = e;
+};
+
+</script>
+
+<template>
+    <p>{{ form.firstName }}</p>
+        <p>{{ form.lastName}}</p>
+        <p>{{ form.Number }}</p>
+    <div class="w-full h-screen flex gap-4 items-center justify-center">
+        <Input :type="type" id="firstName" name="firstName" :placeholder="placeholder" :value="form.firstName" :onChange="onChange"/>
+        <Input type="password" id="lastName" name="lastName" placeholder="Enter last Name " :value="form.lastName" :onChange="onChange"/>
+        <Input type="number" id="number" name="number" placeholder="Enter number " :value="form.number" :onChange="onChange"/> 
     </div>
+
+
 </template>
